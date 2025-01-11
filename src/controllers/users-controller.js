388e4e40c8722e97d74da5143aaacc7b70a55bec
Annylory LR <annyloryrosa@gmail.com>
2025-01-11 13:00:15 -0300

    users-controller

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,14 +1,52 @@
+import jwt from "jsonwebtoken";
 import { db } from "../config/database";
-import { userSigninSchema,userSignupSchema } from "../schemas/userSchema";
-import { schemaValidate } from "../middlewares/schema-middleware";
-
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+dotenv.config()
 
 
 export async function signUp(req, res){
     const user = req.body;
 
-    schemaValidate()
+    try {
+        await db.collection("users").insertOne({
+            ...user, password: bcrypt.hashSync(user.password, 10)
+        })
 
+        return res.status(201).send("Cadastro realizado com sucesso!")
 
+    } catch (error) {
+        return res.status(500).send(error.message)
 
+    }
+}
+
+export async function signIn(req, res){
+    const user = req.body;
+
+    try {
+         const registeredUser = await db.collection("users").findOne({
+            email:user.email
+        });
+
+        if(!registeredUser){
+            return res.status(401).send("Email e senha incompatíveis.")
+        }
+
+        if(registeredUser && bcrypt.compareSync(user.password, registeredUser.password)){
+            console.log("Usuário logado com sucesso!")
+         
+          const token = jwt.sign(
+            {userId: registeredUser._id},
+            process.env.JWT_SECRET,
+            {expiresIn: 86400});
+
+          return res.send(token);
+        }
+
+        return res.status(401).send("Email e senha incompativéis")    
+       
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
 }
