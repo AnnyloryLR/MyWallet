609c2648744d5196e4034fc03ede496aa4be3346
Annylory LR <annyloryrosa@gmail.com>
2025-01-13 10:40:29 -0300

    authentication middleware

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -14,21 +14,20 @@ export async function tokenValidate(req, res, next){
     try {
         jwt.verify(token, process.env.JWT_SECRET, async (error, decoded) => {
             if(error) return res.status(401).send(error);
-        })
+        
+            const user = await db.collection("users").findOne({
+                _id: new ObjectId( decoded.userId)
+            }) 
+            
+            if(!user) return res.sendStatus(401);
 
-        const user = await db.collection("users").findOne({
-            _id: new ObjectId( decoded.userId)
-        });
+            res.locals.user = user;
 
-        if(!user) return res.sendStatus(401);
-
-        res.locals.user = user;
-
-        return next();
+            return next();
+        })      
 
     } catch (error) {
         return res.status(500).send(error.message);
     }
-
 
 }
